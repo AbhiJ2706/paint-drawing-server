58019222583018c587b306067e8b74d46b5dd4f6
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2020-11-10 23:10:40 -0500

    Implemented eraser

--- drawer_js_combined/public/penDrawer.mjs
+++ drawer_js_combined/public/penDrawer.mjs
@@ -3,46 +3,83 @@ class penDrawer {
     constructor(canvas){
         this.c = canvas;
         this.currentCanvas = canvas.getContext("2d");
+        this.currentCanvas.strokeStyle = "#000000"
         this.currentCanvas.fillStyle = "#FFFFFF"
         this.currentCanvas.lineCap = "round";
         this.currentCanvas.fillRect(0, 0, 800, 600)
+        this.currentFillStyle = "#000000"
         this.x = 0;
         this.y = 0;
         this.x2 = 0;
         this.y2 = 0;
         this.drawing = false;
+        this.eraser = false;
+        this.drawingMode = true;
+        this.erasingMode = false;
     }
 
     setDraw(e){
-        var rect = this.c.getBoundingClientRect();
-        this.drawing = true;
-        this.x = e.clientX - rect.left;
-        this.y = e.clientY - rect.top;
+        if (this.drawingMode){
+            var rect = this.c.getBoundingClientRect();
+            this.drawing = true;
+            this.eraser = false;
+            this.x = e.clientX - rect.left;
+            this.y = e.clientY - rect.top;
+        } else {
+            this.drawing = false;
+            this.eraser = true;
+            var rect = this.c.getBoundingClientRect();
+            this.x = e.clientX - rect.left;
+            this.y = e.clientY - rect.top;
+        }
+    }
+
+    setToDrawMode(){
+        this.drawingMode = true;
+        this.erasingMode = false;
+    }
+
+    setToEraserMode(){
+        this.drawingMode = false;
+        this.erasingMode = true;
     }
 
     unSetDraw(){
         this.drawing = false;
+        this.eraser = false;
     }
 
     setThickness(x){
-        this.currentCanvas.strokeWidth = x;
+        this.currentCanvas.lineWidth = x;
     }
 
     draw(e){
-        if (this.drawing){
-            var rect = this.c.getBoundingClientRect();
-            this.x2 = e.clientX - rect.left; 
-            this.y2 = e.clientY - rect.top;
-            this.currentCanvas.beginPath();
-            this.currentCanvas.moveTo(this.x, this.y);
-            this.currentCanvas.lineTo(this.x2, this.y2);
-            this.currentCanvas.stroke(); 
-            this.x = this.x2;
-            this.y = this.y2;
-        }
+        var rect = this.c.getBoundingClientRect();
+        this.x2 = e.clientX - rect.left; 
+        this.y2 = e.clientY - rect.top;
+        this.currentCanvas.strokeStyle = this.currentFillStyle;
+        this.currentCanvas.beginPath();
+        this.currentCanvas.moveTo(this.x, this.y);
+        this.currentCanvas.lineTo(this.x2, this.y2);
+        this.currentCanvas.stroke(); 
+        this.x = this.x2;
+        this.y = this.y2;
+    }
+
+    erase(e){
+        var rect = this.c.getBoundingClientRect();
+        this.x2 = e.clientX - rect.left; 
+        this.y2 = e.clientY - rect.top;
+        this.currentCanvas.strokeStyle = "#FFFFFF";
+        this.currentCanvas.beginPath();
+        this.currentCanvas.moveTo(this.x, this.y);
+        this.currentCanvas.lineTo(this.x2, this.y2);
+        this.currentCanvas.stroke(); 
+        this.x = this.x2;
+        this.y = this.y2;
     }
 
     receiveFillStyle(f){
-        this.currentCanvas.strokeStyle = f;
+        this.currentFillStyle = f;
     }
 }
